e093d52da06b7a2fc72284035a7042ea7ef1cfff
Christopher Quiñonez <58543098+chrisquices@users.noreply.github.com>
2026-05-10 12:09:09 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 
 export default defineConfig({
     plugins: [svelte(), tailwindcss()],
+    base: '/vendor/sentinel/',
     publicDir: false,
     resolve: {
         alias: {
