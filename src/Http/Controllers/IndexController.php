b910750640851c828a04e00fa1f421cd56b0af14
Christopher Quiñonez <58543098+chrisquices@users.noreply.github.com>
2026-05-10 00:04:27 -0300

    update

--- src/Http/Controllers/IndexController.php
+++ src/Http/Controllers/IndexController.php
@@ -2,12 +2,15 @@
 
 namespace Chrisquices\Sentinel\Http\Controllers;
 
+use Chrisquices\Sentinel\Services\SystemService;
 use Illuminate\Routing\Controller;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
+        SystemService::forgetCpuHistory();
+
         return view('sentinel::app');
     }
 }
